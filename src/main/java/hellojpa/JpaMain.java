014718e145bc008f786ace600f0f26a719adff55
namuuCY <wkdcjfdud13@gmail.com>
2024-11-17 13:11:15 +0900

    9-2 proxy 객체의 타입체킹

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -28,8 +28,9 @@ public class JpaMain {
 //            Member findMember = em.find(Member.class, member.getId());
             Member findMember = em.getReference(Member.class, member.getId());
             // 사용을 안하면 SELECT query안나감
-            System.out.println(findMember.getClass());
+            System.out.println(findMember instanceof Member); //true
             // 사용하면 쿼리가 나감.
+
 
             tx.commit();
         } catch (Exception e) {
